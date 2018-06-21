f76a27301291e1e02331eb719c2a0a3fd874402e
Jacob <jakexmueller@gmail.com>
2018-06-21 09:33:39 -0500

    binary tree methods added

--- binarysearchtree/binarysearchtree/BinarySearchTree.cs
+++ binarysearchtree/binarysearchtree/BinarySearchTree.cs
@@ -41,6 +41,108 @@ namespace binarysearchtree
 
             Display(node.leftChild, level + 1);
         }
+        //Pre-Order traversal
+        public void PreOrder(Node node)
+        {
+            if(node == null)
+            {
+                return;
+            }
+            Console.Write(node.data + " ");
+            PreOrder(node.leftChild);
+            PreOrder(node.rightChild);
+        }
+        //In-order traversal
+        public void InOrder()
+        {
+            InOrder(root);
+            Console.WriteLine();
+        }
+        public void InOrder(Node node)
+        {
+            if(node == null)
+            {
+                return;
+            }
+            InOrder(node.leftChild);
+            Console.Write(node.data + " ");
+            InOrder(node.rightChild);
+        }
+        //Post-oredr traversal
+        public void PostOrder()
+        {
+            PostOrder(root);
+            Console.WriteLine();
+        }
+        public void PostOrder(Node node)
+        {
+            if(node == null)
+            {
+                return; 
+            }
+            PostOrder(node.leftChild);
+            PostOrder(node.rightChild);
+            Console.Write(node.data + " ");
+        }
+        //Level-order trversal
+        public void LevelOrder()
+        {
+            if(root == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            Node node;
+            while(queue.Count != 0)
+            {
+                node = queue.Dequeue();
+                Console.Write(node.data + " ");
+                if(node.leftChild != null)
+                {
+                    queue.Enqueue(node.leftChild);
+                }
+                if(node.rightChild != null)
+                {
+                    queue.Enqueue(node.rightChild);
+                }
+            }
+            Console.WriteLine();
+        }
+        //find height of binary tree
+        public int Height()
+        {
+            return Height(root);
+        }
+        public int Height(Node node)
+        {
+            if(node == null)
+            {
+                return 0;
+            }
+            int heightLeft = Height(node.leftChild);
+            int heightRight = Height(node.rightChild);
+
+            if(heightLeft > heightRight)
+            {
+                return 1 + heightRight;
+            }
+            else
+            {
+                return 1 + heightRight;
+            }
+        }
+        public void CreateTree()
+        {
+            root = new Node('P');
+            root.leftChild = new Node('Q');
+            root.rightChild = new Node('R');
+            root.leftChild.leftChild = new Node('A');
+            root.leftChild.rightChild = new Node('B');
+            root.rightChild.leftChild = new Node('X');
+        }
 
         
         
